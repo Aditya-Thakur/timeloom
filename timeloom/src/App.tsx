df4d96091e2162f04f1ab3cffa07af98ed154b55
aditya-thakur <adityathakur532@gmail.com>
2025-03-07 22:11:18 +0530

    First Draft of TimeLoom

---
+++ timeloom/src/App.tsx
@@ -0,0 +1,12 @@
+import TimeLoom from './components/TimeLoom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TimeLoom />
+    </div>
+  );
+}
+
+export default App;
